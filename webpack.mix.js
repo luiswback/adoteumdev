9c2a81587570bacdb9f400fd09f28e9b7e87176b
Luis <luiswback@hotmail.com>
2023-04-28 11:43:17 -0300

    tailwind config and start of home screen

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,8 @@ mix.js("resources/js/app.js", "public/js/app.js")
     .sass("resources/sass/app.scss", "public/css/app.css")
     .tailwind("./tailwind.config.js")
     .sourceMaps();
+    mix.disableSuccessNotifications();
+
 
 if (mix.inProduction()) {
     mix.version();
