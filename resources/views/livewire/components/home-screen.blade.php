546c90d9d5986735e5741f91cc64a936dcfe76f7
Luis <luiswback@hotmail.com>
2023-05-08 21:46:30 -0300

    finish splash screen

--- resources/views/livewire/components/home-screen.blade.php
+++ resources/views/livewire/components/home-screen.blade.php
@@ -1,3 +1,25 @@
 <div>
-    TESTE
+    <div
+        class="flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-tr from-primary-100 to-secondary-100">
+
+        <div class="flex flex-1 flex-col items-center justify-end space-x-2 text-3xl px-10">
+
+            <div class="flex flex-row items-center justify-center space-x-2 text-3xl p-4 mb-24">
+                <img class="w-12 h-12" src="{{asset('assets/mstile-144x144.png')}}" alt="">
+                <span class="text-white font-bold">AdoteUm.Dev</span>
+            </div>
+
+            <p class="text-center text-white text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusantium atque beatae consectetur corporis
+                deleniti deserunt, dignissimos eveniet neque nisi odio optio perspiciatis provident quidem quo
+                repellendus saepe temporibus ullam vero?
+            </p>
+            <button class="bg-white p-4 text-grey-100 mt-8 w-full text-sm rounded-full font-bold transform duration-150 active:scale-90">
+                CRIAR UMA CONTA
+            </button>
+            <button class="border-solid border-white border-2 p-4 text-white mt-4 w-full text-sm rounded-full mb-8 font-bold transform duration-150 active:scale-90">
+                ENTRAR
+            </button>
+        </div>
+    </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
     variants: {
         extend: {
             backgroundColor: ['active'],
+            scale: ['active'],
         }
     },
     purge: {
