3caf146e65a0991d5df4bfcc7e0c43be32415595
Luis <luiswback@hotmail.com>
2023-05-12 16:13:42 -0300

    chore: implementing integration classes and components with github

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\EmailVerificationController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Livewire\Auth\Login;
-use App\Http\Livewire\Auth\Passwords\Confirm;
-use App\Http\Livewire\Auth\Passwords\Email;
-use App\Http\Livewire\Auth\Passwords\Reset;
-use App\Http\Livewire\Auth\Register;
-use App\Http\Livewire\Auth\Verify;
+use App\Http\Controllers\Auth\GithubController;
 use App\Http\Livewire\Components\HomeScreen;
 use App\Http\Livewire\Components\SplashScreen;
 use Illuminate\Support\Facades\Route;
@@ -17,46 +10,10 @@ use Laravel\Socialite\Facades\Socialite;
 Route::get('/', SplashScreen::class)->name('app.splash');
 Route::get('home', HomeScreen::class)->name('app.home');
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', Login::class)
-        ->name('login');
-
-    Route::get('register', Register::class)
-        ->name('register');
-});
-
-
-Route::get('password/reset', Email::class)
-    ->name('password.request');
-
-Route::get('password/reset/{token}', Reset::class)
-    ->name('password.reset');
-
-Route::middleware('auth')->group(function () {
-    Route::get('email/verify', Verify::class)
-        ->middleware('throttle:6,1')
-        ->name('verification.notice');
-
-    Route::get('password/confirm', Confirm::class)
-        ->name('password.confirm');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
-        ->middleware('signed')
-        ->name('verification.verify');
-
-    Route::post('logout', LogoutController::class)
-        ->name('logout');
-});
 
 Route::get('/auth/redirect', function () {
     return Socialite::driver('github')->redirect();
 })->name('socialite.redirect-github');
 
-Route::get('/auth/github', function () {
-    $user = Socialite::driver('github')->user();
+Route::get('/auth/github', GithubController::class);
 
-    // $user->token
-    dd($user, $user->token);
-});
