7ea088920702a973a5d7f44f4182e2c350670999
Luis <luiswback@hotmail.com>
2023-05-29 13:33:55 -0300

    feat: interest  screen

--- app/Http/Livewire/Components/InterestScreen.php
+++ app/Http/Livewire/Components/InterestScreen.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Livewire\Components;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class InterestScreen extends Component
 {
 
     public $user;
+    public $categories;
 
     public function mount()
     {
-        $this->user = auth()->user()->load('profile');
+        $this->user = auth()->user()->load('profile')->toArray();
+        $this->categories = Category::with('skills')->get()->toArray();
     }
 
     public function render()
--- resources/views/livewire/components/interest-screen.blade.php
+++ resources/views/livewire/components/interest-screen.blade.php
@@ -1,7 +1,13 @@
 <div class="flex flex-col min-h-screen items-center w-full p-4">
     <div class="flex flex-col items-center justify-center space-y-2">
-        <img src="{{$user->profile->avatar}}" alt="">
-        <span class="text-grey-100 font-bold">{{$user->name}}</span>
-        <h1 class="text-grey">Complete seus interesses</h1>
+        <img class="inline-block h-20 w-20 rounded-full ring-2 ring-white"
+             src="{{$user['profile']['avatar']}}" alt="">
+        <h2 class="text-base font-semibold text-gray-900">{{$user['name'] . ', complete seus interesses.'}}</h2>
     </div>
+    <form class="w-full">
+       <x-start-form :categories="$categories"/>
+    </form>
 </div>
+
+
+
